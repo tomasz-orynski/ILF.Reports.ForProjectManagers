83aaf38648517390277461a2e4ee99ac9136ef71
Tomasz <tomasz.orynski@gmail.com>
2017-07-04 21:43:44 +0200

    Case ignore in string compare

--- src/BlueBit.ILF.Reports.ForProjectManagers/Utils/KeyComparer.cs
+++ src/BlueBit.ILF.Reports.ForProjectManagers/Utils/KeyComparer.cs
@@ -8,32 +8,32 @@ namespace BlueBit.ILF.Reports.ForProjectManagers.Utils
         T Normalize(T key);
     }
 
-    public class KeyComparer : IKeyComparer<string>
+    public abstract class KeyComparer<T> : IKeyComparer<T>
     {
-        public bool Equals(string x, string y) => string.Compare(x, y, true) == 0;
-
-        public int GetHashCode(string obj) => Normalize(obj).GetHashCode();
-
-        public string Normalize(string key) => key.ToLower();
+        public int GetHashCode(T obj) => Normalize(obj).GetHashCode();
+        public abstract bool Equals(T x, T y);
+        public abstract T Normalize(T key);
     }
-    public class KeyComparerT2 : IKeyComparer<(string a, string b)>
+
+    public class KeyComparer : KeyComparer<string>
     {
-        public bool Equals((string a, string b) x, (string a, string b) y)
+        public override bool Equals(string x, string y) => string.Compare(x, y, true) == 0;
+        public override string Normalize(string key) => key.ToLower();
+    }
+    public class KeyComparerT2 : KeyComparer<(string a, string b)>
+    {
+        public override bool Equals((string a, string b) x, (string a, string b) y)
             => string.Compare(x.a, y.a, true) == 0
             && string.Compare(x.b, y.b, true) == 0;
 
-        public int GetHashCode((string a, string b) obj) => Normalize(obj).GetHashCode();
-
-        public (string a, string b) Normalize((string a, string b) key) => (key.a.ToLower(), key.b.ToLower());
+        public override (string a, string b) Normalize((string a, string b) key) => (key.a.ToLower(), key.b.ToLower());
     }
-    public class KeyComparerT3 : IKeyComparer<(string a, string b, string c)>
+    public class KeyComparerT3 : KeyComparer<(string a, string b, string c)>
     {
-        public bool Equals((string a, string b, string c) x, (string a, string b, string c) y)
+        public override bool Equals((string a, string b, string c) x, (string a, string b, string c) y)
             => string.Compare(x.a, y.a, true) == 0
             && string.Compare(x.b, y.b, true) == 0
             && string.Compare(x.c, y.c, true) == 0;
-
-        public int GetHashCode((string a, string b, string c) obj) => Normalize(obj).GetHashCode();
-        public (string a, string b, string c) Normalize((string a, string b, string c) key) => (key.a.ToLower(), key.b.ToLower(), key.c.ToLower());
+        public override (string a, string b, string c) Normalize((string a, string b, string c) key) => (key.a.ToLower(), key.b.ToLower(), key.c.ToLower());
     }
 }
